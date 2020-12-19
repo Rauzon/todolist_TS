307ccc5830394b03a7b8c123b037a1213c7c63d2
Vladislav <vladby86@gmail.com>
2020-12-20 00:32:37 +0300

    Updates

---
+++ precache-manifest.63ba19f45addc9148b323a82b769b6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a14dbd452ef304b069fa33d3f2c26cb",
+    "url": "/todolist_TS/index.html"
+  },
+  {
+    "revision": "f116b6a0a6a2014f6133",
+    "url": "/todolist_TS/static/css/main.82268206.chunk.css"
+  },
+  {
+    "revision": "5ccd77358c9b7594dfad",
+    "url": "/todolist_TS/static/js/2.63e7bf30.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist_TS/static/js/2.63e7bf30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f116b6a0a6a2014f6133",
+    "url": "/todolist_TS/static/js/main.b1d6bd60.chunk.js"
+  },
+  {
+    "revision": "7db45f67a7e8ea9bb921",
+    "url": "/todolist_TS/static/js/runtime-main.4c814e2b.js"
+  }
+]);
